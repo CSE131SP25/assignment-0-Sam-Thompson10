9e89727db0589cac3af13a48a18c9dbaa9a62ec2
samue <samue@Sam-Dell-Laptop.dhcp.wustl.edu>
2025-01-16 10:53:16 -0600

    Robot Draws and Octagon

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -30,12 +30,20 @@ public class RobotInstructions  {
 		 * as you desire.
 		 */
 		robot.forward(120);
-		robot.forward(-30);
-		robot.forward(-60);
-		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
-		robot.forward(80);
+		robot.turnRight(45);
+		robot.forward(120);
+		robot.turnRight(45);
+		robot.forward(120);
+		robot.turnRight(45);
+		robot.forward(120);
+		robot.turnRight(45);
+		robot.forward(120);
+		robot.turnRight(45);
+		robot.forward(120);
+		robot.turnRight(45);
+		robot.forward(120);
+		robot.turnRight(45);
+		robot.forward(120);
 	}
 
 }
